4e8c9565b4f8fece71427a369a9c60574b9f3fa4
Daniel Lewis <github@mrdaniellewis.co.uk>
2026-01-11 14:40:41 +0000

    final fixes

--- src/password_hasher.js
+++ src/password_hasher.js
@@ -18,33 +18,30 @@ class PasswordHasher extends HTMLElement {
     this.#localStore = new Store({ keys: Object.keys(defaultSettings) });
     this.#updateDataList();
     this.#setClearTimeout();
-    this.#form.masterKey.addEventListener('input', this.#onMasterKeyInput);
-    this.#form.masterKey.addEventListener('change', this.#onMasterKeyChange);
-    this.#form.addEventListener('submit', this.#onSubmit);
-    this.#form.siteTag.addEventListener('change', this.#onSiteTagChange);
-    this.#form.siteTag.addEventListener('input', this.#onSiteTagInput);
-    this.#form.reset.addEventListener('click', this.#onResetClick);
-    this.#form.delete.addEventListener('click', this.#onDeleteClick);
-    this.#form.copy.addEventListener('click', this.#onCopyClick);
-    this.#form.export.addEventListener('click', this.#onExportClick);
-    this.#form.import.addEventListener('click', this.#onImportClick);
-    this.querySelector('#importForm').addEventListener(
-      'submit',
-      this.#onImportSubmit,
-    );
+    this.#id('masterKey').addEventListener('input', this.#onMasterKeyInput);
+    this.#id('masterKey').addEventListener('change', this.#onMasterKeyChange);
+    this.#id('form').addEventListener('submit', this.#onSubmit);
+    this.#id('siteTag').addEventListener('change', this.#onSiteTagChange);
+    this.#id('siteTag').addEventListener('input', this.#onSiteTagInput);
+    this.#id('reset').addEventListener('click', this.#onResetClick);
+    this.#id('delete').addEventListener('click', this.#onDeleteClick);
+    this.#id('copy').addEventListener('click', this.#onCopyClick);
+    this.#id('export').addEventListener('click', this.#onExportClick);
+    this.#id('import').addEventListener('click', this.#onImportClick);
+    this.#id('importForm').addEventListener('submit', this.#onImportSubmit);
     window.addEventListener('focus', () => this.#setClearTimeout());
   }
 
-  get #form() {
-    return this.querySelector('#form');
+  #id(id) {
+    return this.querySelector(`#${CSS.escape(id)}`);
   }
 
   get #settings() {
     return {
-      requirePunctuation: this.#form.elements.requirePunctuation.checked,
-      restrictSpecial: this.#form.elements.restrictSpecial.checked,
-      hashWordSize: this.#form.elements.hashWordSize.valueAsNumber,
-      bangify: this.#form.elements.bangify.checked,
+      requirePunctuation: this.#id('requirePunctuation').checked,
+      restrictSpecial: this.#id('restrictSpecial').checked,
+      hashWordSize: this.#id('hashWordSize').valueAsNumber,
+      bangify: this.#id('bangify').checked,
     };
   }
 
@@ -60,7 +57,7 @@ class PasswordHasher extends HTMLElement {
   };
 
   #onSubmit = (e) => {
-    if (this.#form.checkValidity()) {
+    if (e.target.checkValidity()) {
       e.preventDefault();
       this.#generate();
     }
@@ -68,10 +65,10 @@ class PasswordHasher extends HTMLElement {
 
   #onSiteTagChange = ({ target }) => {
     target.value = target.value.trim();
-    if (!this.#form.checkValidity()) {
+    if (!target.form.checkValidity()) {
       return;
     }
-    const { value } = this.#form.siteTag;
+    const { value } = this.#id('siteTag');
     if (this.#localStore.has(value)) {
       this.#generate();
     }
@@ -86,20 +83,20 @@ class PasswordHasher extends HTMLElement {
   };
 
   #onCopyClick = () => {
-    const hash = this.#form.hash;
+    const hash = this.#id('hash');
     navigator.clipboard.writeText(hash.value);
     hash.focus();
     hash.setSelectionRange(0, hash.value.length);
   };
 
   #onDeleteClick = () => {
-    this.#localStore.delete(this.#form.elements.siteTag.value);
-    this.#form.elements.delete.hidden = true;
+    this.#localStore.delete(this.#id('siteTag').value);
+    this.#id('delete').hidden = true;
     this.#updateDataList();
   };
 
   #updateDataList() {
-    this.#form.querySelector('#savedSites').replaceChildren(
+    this.#id('savedSites').replaceChildren(
       ...[...this.#localStore.keys()]
         .map((key) => [key.toLowerCase(), key])
         .sort()
@@ -114,9 +111,9 @@ class PasswordHasher extends HTMLElement {
   }
 
   #setSaved() {
-    const value = this.#form.siteTag.value.trim();
-    this.#form.hash.value = '';
-    this.#form.hash.classList.remove('ok', 'danger', 'network');
+    const value = this.#id('siteTag').value.trim();
+    this.#id('hash').value = '';
+    this.#id('hash').classList.remove('ok', 'danger', 'network');
 
     if (this.#localStore.has(value)) {
       const settings = this.#localStore.get(value);
@@ -124,18 +121,18 @@ class PasswordHasher extends HTMLElement {
       this.querySelector('#details').open = !Object.entries(
         defaultSettings,
       ).every(([key, value]) => settings[key] === value);
-      this.#form.elements.delete.hidden = false;
+      this.#id('delete').hidden = false;
     } else {
       this.#updateSettings(defaultSettings);
       this.querySelector('#details').open = false;
-      this.#form.elements.delete.hidden = false;
+      this.#id('delete').hidden = false;
     }
   }
 
   async #generate() {
-    const siteTag = this.#form.elements.siteTag.value;
+    const siteTag = this.#id('siteTag').value;
     if (this.#masterKey === siteTag) {
-      this.#form.elements.siteTag.value = '';
+      this.#id('siteTag').value = '';
       return;
     }
     const settings = this.#settings;
@@ -144,7 +141,7 @@ class PasswordHasher extends HTMLElement {
       siteTag,
       ...settings,
     });
-    const { hash: hashInput } = this.#form.elements;
+    const hashInput = this.#id('hash');
     hashInput.value = hash;
     hashInput.focus();
     hashInput.setSelectionRange(0, hash.length);
@@ -175,11 +172,10 @@ class PasswordHasher extends HTMLElement {
   }
 
   #updateSettings(settings) {
-    this.#form.elements.requirePunctuation.checked =
-      settings.requirePunctuation;
-    this.#form.elements.restrictSpecial.checked = settings.restrictSpecial;
-    this.#form.elements.hashWordSize.value = settings.hashWordSize;
-    this.#form.elements.bangify.checked = settings.bangify;
+    this.#id('requirePunctuation').checked = settings.requirePunctuation;
+    this.#id('restrictSpecial').checked = settings.restrictSpecial;
+    this.#id('hashWordSize').value = settings.hashWordSize;
+    this.#id('bangify').checked = settings.bangify;
   }
 
   #setClearTimeout() {
@@ -187,12 +183,8 @@ class PasswordHasher extends HTMLElement {
     this.#clearTimeout = window.setTimeout(
       () => {
         this.masterKey = null;
-        this.#form.elements.masterKey.value = '';
-        this.#form.elements.masterKey.classList.remove(
-          'ok',
-          'danger',
-          'network',
-        );
+        this.#id('masterKey').value = '';
+        this.#id('masterKey').classList.remove('ok', 'danger', 'network');
       },
       4 * 60 * 60 * 1000,
     );
